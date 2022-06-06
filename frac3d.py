82ffbd2b94591216b700770bb96ff8f1b597c215
zhangruihan1 <47313357+zhangruihan1@users.noreply.github.com>
2022-06-06 22:09:08 +0800

    Update frac3d.py

--- frac3d.py
+++ frac3d.py
@@ -7,7 +7,7 @@ class FRAC3D(nn.Module):
 		super(FRAC3D, self).__init__()
 		resnet_model = models.resnet50(num_classes=1200)
 		resnet_model.conv1 = nn.Conv2d(4, 64, kernel_size=7, stride=2, padding=3, bias=False) 
-		resnet_model.load_state_dict(torch.load('ckpt_path', map_location=torch.device('cpu')))
+		resnet_model.load_state_dict(torch.load(ckpt_path, map_location=torch.device('cpu')))
 		self.truncated_resnet = nn.Sequential(*list(resnet_model.children())[:-1])
 	def forward(self, x):
 		feats = self.truncated_resnet(x)
