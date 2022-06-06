1e1caba16c7ae87ea54bbab4643dd8bd57b89906
zhangruihan1 <47313357+zhangruihan1@users.noreply.github.com>
2022-06-06 22:05:13 +0800

    Update frac3d.py

--- frac3d.py
+++ frac3d.py
@@ -3,7 +3,7 @@ from torchvision import models
 
 class FRAC3D(nn.Module):
 	def __init__(self, ckpt_path = None):
-		super(FRAC, self).__init__()
+		super(FRAC3D, self).__init__()
 		resnet_model = models.resnet50(num_classes=1200)
 		resnet_model.conv1 = nn.Conv2d(4, 64, kernel_size=7, stride=2, padding=3, bias=False) 
 		resnet_model.load_state_dict(torch.load('ckpt_path', map_location=torch.device('cpu')))
