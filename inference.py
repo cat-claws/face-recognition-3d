ac1ef0043581d8b25bfe205a1011d5d679ed7e06
zhangruihan1 <47313357+zhangruihan1@users.noreply.github.com>
2022-06-09 00:55:19 +0800

    Update inference.py

--- inference.py
+++ inference.py
@@ -5,10 +5,10 @@ from frac3d import FRAC3D
 
 def get_3d_model(checkpoint):
 	if 'resnet50' in checkpoint.lower():
-		model = FRAC3D("RGB-D-ResNet50-from-imagenet.pkl")
+		model = FRAC3D(checkpoint) # RGB-D-ResNet50-from-imagenet.pkl
 	elif 'led' in checkpoint.lower():
-		model = Led3D("led3d.pth")
+		model = Led3D(checkpoint) # led3d.pth
 	elif 'fr3dnet' in checkpoint.lower():
-		model = FR3DNET("fr3dnet.pth")
+		model = FR3DNET(checkpoint) # fr3dnet.pth
 	
 	return model.eval()
