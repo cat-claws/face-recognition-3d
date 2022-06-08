f10c05b0f1b7845466304f941139e993ffd1cb2c
zhangruihan1 <47313357+zhangruihan1@users.noreply.github.com>
2022-06-09 00:51:08 +0800

    Create inference.py

---
+++ inference.py
@@ -0,0 +1,14 @@
+import torch
+from fr3dnet import FR3DNET
+from led3d import Led3D
+from frac3d import FRAC3D
+
+def get_3d_model(checkpoint):
+	if 'resnet50' in checkpoint.lower():
+		model = FRAC3D("RGB-D-ResNet50-from-imagenet.pkl")
+	elif 'led' in checkpoint.lower():
+		model = Led3D("led3d.pth")
+	elif 'fr3dnet' in checkpoint.lower():
+		model = FR3DNET("fr3dnet.pth")
+	
+	return model.eval()
